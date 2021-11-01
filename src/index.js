036c0b569736be4ecccf286631c97ca27538fb47
Akhil Kamsala <87219018+akhilsdeportfolio@users.noreply.github.com>
2021-11-01 23:28:29 +0530

    custom theme

--- src/index.js
+++ src/index.js
@@ -3,10 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+
+
+
+
+
+
+const themeOptions = {
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#FF3D5A',
+      light: '#ff546d',
+    },
+    secondary: {
+      main: '#c31761',
+      light:'#FFB3D4'
+    },
+    verylight: {
+      main: '#FFB3D4'
+    },
+  },
+};
+
+const theme=createTheme(themeOptions);
 
 ReactDOM.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
